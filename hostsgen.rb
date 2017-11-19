7d62db6b696f8d2b0154089dba0bac1fa36285c8
duangsuse <fedora-opensuse@outlook.com>
2017-11-19 22:50:48 +0800

    Style: format hostsgen.rb

--- hostsgen.rb
+++ hostsgen.rb
@@ -19,53 +19,54 @@
 #########################################################################
 
 VERSION = "0.1.3"
-CFG_FILENAME = "hostsgen.yml"
-MOD_FILENAME = "mod.txt"
-HEAD_FILENAME = ENV['HOSTSGEN_HEAD_TEXT']||"head.txt"
-OUTPUT_EVAL = ENV['HOSTSGEN_OUTPUT_EVAL']||"@loc + ' ' + @host"
+CFG_FILENAME = ENV["HOSTSGEN_CFG"] || "hostsgen.yml"
+MOD_FILENAME = ENV["HOSTSGEN_MOD"] || "mod.txt"
+HEAD_FILENAME = ENV['HOSTSGEN_HEAD'] || "head.txt"
+OUTPUT_EVAL = ENV['HOSTSGEN_EVAL'] || "@loc + ' ' + @host"
+
 # valid hostname may contain ASCII char A-Z, a-z, 0-9 and '.', '-'.
-HOSTNAME_VALID_CHARS = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890-."
+HOSTNAME_VALID_CHARS = ENV['HOSTSGEN_VALID_CHARS'] || "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890-."
 
 # main function
 def start(args)
   options = CmdlineOptions.new(args)
-  if !options.silent then
+  if !options.silent
     print "Hostsgen v" + VERSION + "; "
     puts case options.operate
-      when 0; "building project..."
-      when 1; "checking hosts data..."
-      when 2; "cleaning..."
-      when 3; "printing help..."
-      when 4; "printing version..."
-    end
+         when 0; "building project..."
+         when 1; "checking hosts data..."
+         when 2; "cleaning..."
+         when 3; "printing help..."
+         when 4; "printing version..."
+         end
     if options.out then puts "[INFO] Outputting to " + options.out + " ..." end
     if options.no_comments then puts "[INFO] No comments in output file" end
     if options.mod_black_list.length != 0 then print "[INFO] No compile: "; puts options.mod_black_list.to_s end
   end
-  if options.operate == 3 then
+  if options.operate == 3
     puts "Usage: ", $0 + " [build/check/clean/help/version] (args)", "args: -q:quiet -o:out [file] -t:no comments -b(an) [mod]"; exit
   end
   if options.operate == 4 then puts VERSION; exit end
   project_cfg = ProjectConfig.new(options.silent)
-  if options.operate == 2 then
+  if options.operate == 2
     begin
       if not options.out.nil? then File.delete options.out end
       if not project_cfg.out.nil? then File.delete project_cfg.out end
-      rescue
+    rescue
       # nil.to_s == ''
-      if File.exists? options.out.to_s or File.exists? project_cfg.out.to_s then
+      if File.exists? options.out.to_s or File.exists? project_cfg.out.to_s
         puts "[WARN] failed to delete some file"
       end
     end
-    if not (File.exists? options.out.to_s or File.exists? project_cfg.out.to_s) then
+    if not(File.exists? options.out.to_s or File.exists? project_cfg.out.to_s)
       puts "[INFO] Cleaned."
     end
     exit 0
   end
-  if options.operate == 1 then
-    if File.exists? options.out.to_s or File.exists? project_cfg.out.to_s then
+  if options.operate == 1
+    if File.exists? options.out.to_s or File.exists? project_cfg.out.to_s
       hosts = Hosts.new
-      if File.exists? name=options.out.to_s then
+      if File.exists? name = options.out.to_s
         puts "[CHECK] Checking file " + name
         f = File.open name
         hosts.parse(f.read)
@@ -82,7 +83,7 @@ def start(args)
     end
     exit 0
   end
-  if !options.silent then
+  if !options.silent
     print "[INFO] Project '"
     print project_cfg.name
     print "' by "
@@ -97,8 +98,8 @@ def start(args)
   mods = ProjectModules.new(options.silent, project_cfg.mods, options.mod_black_list)
   print "[COMPILE] Modules: "
   puts mods.mods.to_s
-  # if String|nil ...
-  if name=options.out then
+  # if String|nil then ...
+  if name = options.out
     mods.build options.silent, options.no_comments, name, project_cfg
   else
     mods.build options.silent, options.no_comments, project_cfg.out, project_cfg
@@ -125,21 +126,22 @@ class CmdlineOptions
       if i.start_with? "-b" then @mod_black_list.push cmdline[s + 1] end
     end
     @operate = case cmdline[0]
-      when "build"; 0
-      when "check"; 1
-      when "clean"; 2
-      when "help"; 3
-      when "version"; 4
-      else 0
-    end
+               when "build"; 0
+               when "check"; 1
+               when "clean"; 2
+               when "help"; 3
+               when "version"; 4
+               else 0
+               end
   end
-  #getter
+
   attr_reader :mod_black_list
   attr_reader :operate
   attr_reader :silent
   attr_reader :no_comments
+
   def out
-    if !@out.nil?;
+    if !@out.nil?
       if @out.start_with? '-'; puts "[ERR] Output filename should not start with -"; exit 3 end
       if File.directory? @out; puts "[ERR] Cannot use dir as output"; exit 2 end
     end
@@ -153,14 +155,14 @@ class ProjectConfig
     require 'yaml'
     if not File.exist? CFG_FILENAME; puts "[ERR] Project config does not exists"; exit 1 end
     cfg = YAML.load_file(CFG_FILENAME)
-    if !silent then puts "[VERBOSE] Parsed YAML:",cfg.inspect end
+    if !silent then puts "[VERBOSE] Parsed YAML:", cfg.inspect end
     @name = cfg["name"]
     @desc = cfg["desc"]
     @out = cfg["out"]
     @authors = cfg["authors"]
     @mods = cfg["mods"]
   end
-  #getter
+
   attr_reader :name
   attr_reader :desc
   attr_reader :out
@@ -175,20 +177,20 @@ class ProjectModules
     @descs = mods.dup
     # strip desc in module config
     mods.each_with_index do |m, i|
-     space_idx = m.index ' '
-     if space_idx.nil? and not quiet then puts "[WARN] No description in mod " + m
-     else @mods[i] = m[0..space_idx - 1] end
+      space_idx = m.index ' '
+      if space_idx.nil? and not quiet then puts "[WARN] No description in mod " + m       else @mods[i] = m[0..space_idx - 1] end
     end
     dm = @mods & ignored
     @mods = @mods - ignored
-    for m in dm do
+    for m in dm
       @descs.each_with_index do |d, i|
         @descs.delete_at i if d.include? m.to_s
       end
     end
   end
+
   def build(quiet, no_comments, out, cfg)
-    if not quiet then
+    if not quiet
       puts "[COMPILE] Outputting to " + out + (" no comments" if no_comments).to_s
     end
     gen = Hosts.new
@@ -207,20 +209,22 @@ class ProjectModules
     rescue
       puts "[WARN] Cannot put project props"
     end
+    file.puts "#Modules: " + @descs.to_s + "\n"
     @mods.each_with_index do |m, i|
       puts "[COMPILE] Compiling Module #" + i.to_s + ": " + m if not quiet
-      if File.exist? m + '/' + MOD_FILENAME then
+      if File.exist? m + '/' + MOD_FILENAME
         f = File.open m + '/' + MOD_FILENAME
         HostsModule.new(f.read).compile m, file, @descs[i]
-      else puts "[ERR] Cannot find module config"; exit 5 end
+      else puts "[ERR] Cannot find module config"; exit 5       end
     end
+    file.puts "#End modified hosts" + "\n"
   end
-  #getter
+
   attr_reader :mods
 end
 
 # hostsgen module structure&parser
-# contains file names, descriptions, generate rules  
+# contains file names, descriptions, generate rules
 class HostsModule
   def initialize(cfg)
     @files = []
@@ -233,15 +237,16 @@ class HostsModule
       end
     end
   end
+
   def compile(m, file, desc)
     file.puts "#Module: " + m + " : " + desc + "\n" if not ARGV.include? "-t"
-    for f in @files do
+    for f in @files
       begin
         l = f.compile m, file
       rescue => e
         puts "[COMPILE] Failed to compile file: " + e.to_s; exit 7
       end
-        puts " OK, " + l.to_s + " logs generated." if not ARGV.include? "-q"
+      puts " OK, " + l.to_s + " logs generated." if not ARGV.include? "-q"
     end
     file.puts "#endModule: " + m + "\n" if not ARGV.include? "-t"
   end
@@ -261,8 +266,8 @@ class FileConfig
     if desc_starts.nil? then puts "[COMPILE] WARN: Cannot find description start" end
     if desc_starts.nil? then puts "[COMPILE] WARN: Cannot find description end" end
     if not desc_starts.nil? and desc_ends.nil? then raise "[COMPILE] ERR: Endless description (missing ')')" end
-    if not (desc_starts.nil? or desc_ends.nil?) then  @desc = line[desc_starts + 1..desc_ends -1] end
-    if desc_ends.nil? then
+    if not(desc_starts.nil? or desc_ends.nil?) then @desc = line[desc_starts + 1..desc_ends - 1] end
+    if desc_ends.nil?
       @genrule = line[file_ends + 1..line.length]
     else
       @genrule = line[desc_ends + 1..line.length]
@@ -273,6 +278,7 @@ class FileConfig
       raise "error initializing genrule: " + e.to_s
     end
   end
+
   # raise a string contains filename, reason
   # return Hosts data
   def compile(m, f)
@@ -286,7 +292,7 @@ class FileConfig
     end
     f.puts "#FILE: " + @file + " : " + @desc + "\n" if not ARGV.include? '-t'
     f.puts hosts
-    return hosts.logs.length 
+    return hosts.logs.length
     f.puts "#FILE: " + @file + "\n" if not ARGV.include? '-t'
   end
 end
@@ -301,19 +307,20 @@ class GenerateRule
     @host_insert_idx = @host.index "{HOST}"
     @host = @host.tr "{HOST}", "" #blank String is nil in Ruby
     @loc_insert_idx = @loc.index "{IP}"
-    @loc = @loc.tr "{IP}", "" 
+    @loc = @loc.tr "{IP}", ""
     @put_in_host = nil
-    if a=@host_insert_idx.nil? or @loc_insert_idx.nil? then
+    if a = @host_insert_idx.nil? or @loc_insert_idx.nil?
       @put_in_host = !a
       raise "must be one format argument valid ({IP} {HOST}) at least" if a and @loc_insert_idx.nil?
     end
   end
+
   # process a host item using rule
   # return processed item
   def process(hostsitem)
     item = HostsItem.new nil, nil, nil
-    if not @put_in_host.nil? then
-      if @put_in_host then
+    if not @put_in_host.nil?
+      if @put_in_host
         tmp = @host.dup
         tmp.insert @host_insert_idx, hostsitem.loc
         item.set tmp, @loc, hostsitem.line
@@ -340,13 +347,14 @@ class Hosts
   def initialize()
     @logs = []
   end
+
   # parse a String, store data in self
-  # valid log should not be started with #
+  # valid log should not be started with '#'
   def parse(hosts)
     hosts.lines.each_with_index do |l, i|
       l = l.strip
-      if l[0] == '#' then ; next end
-      if l == "" then ; next end
+      if l[0] == '#' then next end
+      if l == "" then next end
       l = l.split ' '
       #raise "more or less than 2 col at line " + i.to_s + " (hostsgen does not trim non-line comments)(plese use space to split only)" if not l.length == 2
       host = l[1]
@@ -354,22 +362,27 @@ class Hosts
       @logs.push (HostsItem.new i, host, loc)
     end
   end
+
   # lint hosts data
   def check
     lint(@logs)
   end
+
   # merges self with other
   def push(other)
     push_logs other
   end
+
   def to_s()
     r = String.new
-    for l in @logs do
+    for l in @logs
       r += l.to_s + "\n"
     end
     return r
   end
+
   def push_logs(o); @logs.push o end
+
   attr_reader :logs
 end
 
@@ -379,16 +392,16 @@ class HostsItem
     @host = host #hostname
     @loc = loc #address
   end
+
   def to_s()
-    #if @loc.nil? or @host.nil? then return @loc.to_s||@host.to_s end
     return eval OUTPUT_EVAL
   end
-  #getter
+
   attr_reader :line
   attr_reader :host
   attr_reader :loc
-  #setter
-  def set(h,i,l)
+
+  def set(h, i, l)
     @line = l
     @host = h
     @loc = i
@@ -399,18 +412,18 @@ end
 # LOC rules (only IPv4 is supported in this file):
 # if not l.start_with? "::" then l.assert_in_pattern [0-255].[0-255].[0-255] end
 # NAME rules:
-# name.assert_only_include HOSTNAME_VALID_CHARS 
+# name.assert_only_include HOSTNAME_VALID_CHARS
 def lint(logs)
   require 'ipaddr'
   logs.each_with_index do |l, i|
     hostname_not_valid = false
     puts "[LINT] WARN: log #" + i.to_s + " may not have a valid IP Address (" + l.loc + ")" if !(IPAddr.new(l.loc) rescue false)
-    for c in l.host.to_s.chars do
+    for c in l.host.to_s.chars
       if not HOSTNAME_VALID_CHARS.include? c then hostname_not_valid = true; cha = c end
     end
     puts "[LINT] WARN: log #" + i.to_s + " may not have a valid hostname (invalid char '" + cha + "')" if hostname_not_valid
   end
 end
 
-
+# invokes main function if this script is running not as a library
 if $0 == __FILE__ then start(ARGV) end
